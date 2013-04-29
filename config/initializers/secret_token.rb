5ec33ede6ee5ea75dd22a6d8e3cdc83e6a4151e0
chickswagger <nomsapn@gmail.com>
2013-04-29 09:20:31 +0200

    commiting the files to our repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsProject::Application.config.secret_token = '6b9e76d68d5899e2f0c567991defe3aeac51c2c97cafda5ba7e5bb6fa5064857983710faeef688a802c3dca6af1e659c5ab9c769cd065bca3b3c85f19257f325'
